ab1690ce01dfbb094e389a987f5d7a15a3113bed
nathaniel <hargraveten@msn.com>
2016-09-14 12:01:55 -0700

    Updated and moved copyright notice

--- README.md
+++ README.md
@@ -20,17 +20,3 @@ This is a temperature and humidity sensor that displays the information on a 16x
  * ground to ground
 
 For this code to work you will first have to move the libraries folder supplied with this into your sketchbook folder so the Arduino IDE can use the libraries and then restart the IDE (for more information on installing libraries see http://arduino.cc/en/Guide/Libraries)
-
- This code is free software; you can redistribute it and/or
- modify it under the terms of the GNU Lesser General Public
- License as published by the Free Software Foundation; either
- version 2.1 of the License, or (at your option) any later version.
-
- This code is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- Lesser General Public License for more details.
-
- You should have received a copy of the GNU Lesser General Public
- License along with this library; if not, write to the Free Software
- Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
--- Temperature_and_humidity_sensor.ino
+++ Temperature_and_humidity_sensor.ino
@@ -1,7 +1,24 @@
 /*
-this is a sketch for a temperature and humidity sensor that displays
-the information on a 16x2 LCD  For more information see the README file supplied with this sketch.
-*/
+ * this is a sketch for a temperature and humidity sensor that displays
+ * the information on a 16x2 LCD  For more information see the README file supplied with this sketch.
+ * 
+ * Copyright (c) 2016 Nathaniel Hargrave. 
+ *
+ * This code is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This code is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
 #include "DHT.h" // include the libraries
 #include <LiquidCrystal.h>
 
